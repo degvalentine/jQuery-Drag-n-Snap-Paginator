d1eada00a66b64c8215080e610bf429698a9acbb
Degory Valentine <degvalentine@gmail.com>
2010-10-11 10:32:56 -0400

    Added support for tables.

--- jquery.drag-n-snap-paginator.js
+++ jquery.drag-n-snap-paginator.js
@@ -10,6 +10,7 @@
  * TODO button location on top/bottom/both
  * TODO resolve button functionality with and without jQuery-UI 
  * TODO should this widget take a width, or just use info from DOM style?
+ * TODO should this widget take a data array and factory function to make children from? (could be runtime improvement over large async-fetched data
  * TODO choose scroll axis x/y
  * TODO make slide in initially to let user know interaction exists (configurable)
  */
@@ -34,20 +35,62 @@
 			}
 		});
 		
-		function initWidget(widget, options) {
+		function initWidget(target, options) {
 			
 			var defaults = {  
-				width: widget.width(),  
+				width: target.width(),  
 				pageCount: 5,
 				buttons: true,
 				itemClassName: 'list-item',
 				pageClassName: 'page',
-				pageSpacing: 100
+				pageSpacing: 100,
+				pageElement: 'div',
+				commonElements: []
 			};
+			
+			var widget, items;
+			
+			// tables have special treatment
+			if (target.is('table')) {
+				defaults.pageElement = 'table';
+				
+				// make widget a div
+				widget = $('<div/>')
+							.attr('id', target.attr('id'))
+							.attr('class', target.attr('class'));
+				
+				// make columns from thead
+				if (target.has('thead').length > 0) {
+					var thead = $('<table/>').appendTo(widget);
+					target.children('thead').find('th').each(function(){
+						$(this).css('width', $(this).width()+'px');
+						defaults.commonElements.push($('<col/>').width($(this).width())); // TODO copy all other th attrs
+					});
+					target.children('thead').find('th').appendTo(thead);
+//					target.children('thead').remove();
+				}
+				
+				// make items from tbody
+				if (target.children('tbody').length > 0) {
+					items = target.children('tbody').children('tr');
+//					target.children('tbody').remove();
+				} else {
+					items = target.children('tr');
+				}
+				
+				target.replaceWith(widget);
+				
+			// everything else
+			} else {
+				widget = target;
+				items = target.children();
+			}
+			
+			
+			
+			
 		    
 			var options = $.extend(defaults, options); 
-			
-			var items = widget.children();
 			
 			// init widget
 			widget.data('page', 0)
@@ -71,7 +114,8 @@
 				   
 		        // paginate
 		        if (i % options.pageCount == 0) {
-		            page = $('<div/>');
+		            page = $('<'+defaults.pageElement+'/>');
+		            $(defaults.commonElements).each(function(){$(this).clone().appendTo(page)});
 		            page.addClass(options.pageClassName)
 		                .width(options.width)
 		                .css('float', 'left')
