a731576824fa22e0c6105d264d7464559888209a
Degory Valentine <degvalentine@gmail.com>
2010-11-05 13:15:49 -0400

    Main function now handles single items differently to allow better usage of return values.

--- jquery.drag-n-snap-paginator.js
+++ jquery.drag-n-snap-paginator.js
@@ -3,7 +3,7 @@
  * 
  * Paginates list into theme-rollable pages. 
  * 
- * TODO add item access functions, such as widget.paginate('get', page, index)
+ * TODO add item access functions, such as widget.paginate('get', page, index)?
  * TODO should this widget take a width, or just use info from DOM style?
  * TODO should this widget take a data array and factory function to make children from? (could be runtime improvement over large async-fetched data
  * TODO choose scroll axis x/y
@@ -15,17 +15,26 @@
  * TODO add flick? scroll speed would start at release velocity and decline till it snaps
  */
 (function($){  
-$.fn.paginate = function(arg, arg2) {
+$.fn.paginate = function() {
 	
-	return this.each(dispatch);
+	// single widget
+	if (this.length == 1) {
+		return dispatch($(this[0]), arguments);
+	}
+	
+	// collection of widgets
+	var r = {};
+	for (i in this) {
+		r[i] = dispatch(this[i], arguments);
+	}
+	return r;
 	
 	/**
 	 * Dispatches to method or initializes widget.
 	 */
-	function dispatch(i, obj) {
-		widget = $(obj);
+	function dispatch(widget, args) {
 		if (widget.data('isPaginated')) {
-			switch (arg) {
+			switch (args[0]) {
 				case 'prev':
 					if (widget.data('has')(widget.data('page') - 1))
 						return widget.data('goto')(widget.data('page') - 1);
@@ -35,21 +44,18 @@ $.fn.paginate = function(arg, arg2) {
 						return widget.data('goto')(widget.data('page') + 1);
 					break;
 				case 'goto':
-					return widget.data('goto')(arg2);
+					return widget.data('goto')(args[1]);
 				case 'has':
-					return widget.data('has')(arg2);
+					return widget.data('has')(args[1]);
 				case 'count':
 					return widget.data('pageCount');
 				case 'page':
-					if ($.type(arg2) == 'number')
-						return widget.data('goto')(arg2);
 					return widget.data('page');
-				
-				// TODO throw/error on default?
 			}
+			// TODO throw/error on default?
 			return widget;
 		} else {
-			return initWidget(widget, arg);
+			return initWidget(widget, args[0]);
 		}
 	}
 	
@@ -57,7 +63,6 @@ $.fn.paginate = function(arg, arg2) {
 	 * Initializes widget.
 	 */
 	function initWidget(widget, options) {
-		
 		var defaults = {  
 			width: widget.width(),  
 			itemsPerPage: 5,
@@ -88,8 +93,8 @@ $.fn.paginate = function(arg, arg2) {
 					header = $(this);
 					header.css('width', header.width()+'px');
 					defaults.commonElements.push($('<col/>').width(header.width())); // TODO copy all other th attrs?
-					header.appendTo(headerRow);
 				});
+				headerRow.append(headers);
 			}
 			widget.children('thead').remove();
 			widget.find('tr:first').has('th').remove();
@@ -150,7 +155,7 @@ $.fn.paginate = function(arg, arg2) {
 		});
 		
 		function hasPage(i) {
-			return i >= 0 && i < widget.data('pageCount');
+			return (i >= 0 && i < widget.data('pageCount'));
 		}
 		function goToPage(i) {
 			if (!hasPage(i)) {
